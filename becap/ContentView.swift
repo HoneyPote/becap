3ea52b72615ce505994100b0645133d7fe4a6087
Adam <adam.mabrouki@webwag.com>
2025-07-15 15:42:08 +0200

    fiiiiiiiirst commit

--- becap/ContentView.swift
+++ becap/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("yooo wtf its working!")
         }
         .padding()
     }
