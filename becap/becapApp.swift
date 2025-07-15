cc2fb63e1dc41838892fd42ae20713b33c40e10b
Victor Derveaux <victorderveaux@MacBook-Pro-de-Victor-2.local>
2025-07-15 15:01:08 +0200

    project start

---
+++ becap/becapApp.swift
@@ -0,0 +1,17 @@
+//
+//  becapApp.swift
+//  becap
+//
+//  Created by Victor Derveaux on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct becapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ becapTests/becapTests.swift
@@ -0,0 +1,17 @@
+//
+//  becapTests.swift
+//  becapTests
+//
+//  Created by Victor Derveaux on 15/07/2025.
+//
+
+import Testing
+@testable import becap
+
+struct becapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
