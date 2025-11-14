74f5d2a9df43f2051e2c747388d67788aeddaf36
Adam <adam.mabrouki@webwag.com>
2025-11-14 21:50:49 +0100

    hide key from git

---
+++ becap/Managers/BecapSecrets.swift.swift
@@ -0,0 +1,12 @@
+//
+// BecapSecrets.swift
+//  becap
+//
+//  Created by Adam Mabrouki on 14/11/2025.
+//
+
+import Foundation
+
+enum BecapSecrets {
+    static let oneSignalRestApiKey = "os_v2_app_ldirud6pczcvlmsyzfgwv6qk6nimepdfxkuunduob6et6w6estsgkk5bjf4jwz3yt6onzeuloi4kuumpcsgrbig36sjhjfayerqnaca"
+}
--- becap/Services/NotificationService.swift
+++ becap/Services/NotificationService.swift
@@ -19,7 +19,7 @@ final class NotificationService {
     // MARK: - OneSignal constants
     private let onesignalAppId = "58d11a0f-cf16-4555-b258-c94d6afa0af3"
 
-    private let onesignalRestAuth = "os_v2_app_ldirud6pczcvlmsyzfgwv6qk6my5syn4bxbexeupzkkmwzd5xb2q3egdpxkhdlnx7aiu27pptpe55ntibtoztsa7qgnjc2twcuqqusi"
+    private let onesignalRestAuth = BecapSecrets.oneSignalRestApiKey
 
     init(db: Firestore = .firestore(), userManager: UserManager = .shared) {
         self.db = db
