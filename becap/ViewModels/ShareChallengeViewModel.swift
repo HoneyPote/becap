a8d8cee20a1dc6446b0691543407f30045fbe3a6
Adam <adam.mabrouki@webwag.com>
2025-11-29 12:04:58 +0100

    FIx bug

--- becap/ViewModels/ShareChallengeViewModel.swift
+++ becap/ViewModels/ShareChallengeViewModel.swift
@@ -85,7 +85,7 @@ final class ShareChallengeViewModel: ObservableObject {
         }
 
         do {
-            let joinedChallenge = try await challengeManager.joinChallenge(withCode: trimmedCode)
+            var joinedChallenge = try await challengeManager.joinChallenge(withCode: trimmedCode)
             let resolvedChallenge = challengeManager.challenges.first(where: { $0.id == joinedChallenge.id }) ?? joinedChallenge
 
             await MainActor.run {
