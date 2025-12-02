ffbfdea70aca2b41679665280dfb5e4e94a8415c
Adam <adam.mabrouki@webwag.com>
2025-12-02 12:56:47 +0100

    Squashed commit of the following:
    commit 61c51e711e90acabf112dff86c1ea882db2196ac
    Author: Adam <adam.mabrouki@webwag.com>
    Date:   Tue Dec 2 12:56:12 2025 +0100
    
        fix optionale issue
    
    commit 83fac640706f9c0a1d6cee7861590165f7ba70ba
    Author: HoneyPote <honeypote.va@gmail.com>
    Date:   Tue Dec 2 12:35:15 2025 +0100
    
        Fix progress fetch resolution
    
    commit 42f25b3922e97766cb1805810627bf071a32b595
    Author: HoneyPote <honeypote.va@gmail.com>
    Date:   Tue Dec 2 11:30:39 2025 +0100
    
        Restore automatic joker consumption

--- becap/Managers/ChallengeManager.swift
+++ becap/Managers/ChallengeManager.swift
@@ -402,17 +402,29 @@ extension ChallengeManager {
 
         try setUserProgress(userId: userId, challengeId: challenge.id, progress: userProgress)
     }
-
     func fetchParticipantsProgress(for challengeId: String) async throws -> [ParticipantProgress] {
         var progresses = try await challengeService.fetchParticipantsProgress(for: challengeId)
 
-        guard let challenge = challenge(for: challengeId) ?? (try await fetchChallenge(by: challengeId)) else {
+
+        let resolvedChallenge: Challenge
+        if let local = challenge(for: challengeId) {
+            resolvedChallenge = local
+        } else if let fetched = try await fetchChallenge(by: challengeId) {
+            resolvedChallenge = fetched
+        } else {
+            // si le challenge n'existe plus, on renvoie juste les progresses
             return progresses
+           
+            // throw ChallengeManagerError.challengeNotFound
         }
 
+        // Étape 2 : auto-déclaration pour le user courant
         if let currentUserId = currentUser?.id,
            let index = progresses.firstIndex(where: { $0.id == currentUserId }),
-           let updatedProgress = await autoDeclareMissedDayIfNeeded(for: challenge, progress: progresses[index]) {
+           let updatedProgress = await autoDeclareMissedDayIfNeeded(
+                for: resolvedChallenge,
+                progress: progresses[index]
+           ) {
             progresses[index] = updatedProgress
         }
 
