6541f6ff9d26a8d5e48d1d7fe54610e597f1e339
HoneyPote <honeypote.va@gmail.com>
2025-12-13 19:28:35 +0100

    Fix challenge tap handling when unlocked

--- becap/Views/Home/Challenge/DefiCell.swift
+++ becap/Views/Home/Challenge/DefiCell.swift
@@ -77,9 +77,11 @@ struct DefiCell: View {
             }
         }
         .padding(4)
-        .onTapGesture {
+        .overlay {
             if isLocked {
-                onLockedTap?()
+                Color.clear
+                    .contentShape(RoundedRectangle(cornerRadius: 18))
+                    .onTapGesture { onLockedTap?() }
             }
         }
     }
