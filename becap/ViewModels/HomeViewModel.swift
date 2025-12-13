49ef77bf4de6a1b333a0ae568dbb59ab90439c15
HoneyPote <honeypote.va@gmail.com>
2025-12-14 00:12:07 +0100

    Fix premium lock check return value

--- becap/ViewModels/HomeViewModel.swift
+++ becap/ViewModels/HomeViewModel.swift
@@ -143,7 +143,7 @@ class HomeViewModel: ObservableObject {
             return true
         }
 
-        challenge.isLocked(for: challengeManager.currentUser?.id, hasPremium: hasPremium)
+        return challenge.isLocked(for: challengeManager.currentUser?.id, hasPremium: hasPremium)
     }
 }
 
