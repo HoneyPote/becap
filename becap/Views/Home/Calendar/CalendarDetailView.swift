57d60900023f763c58e1246075199fb7049eab28
HoneyPote <honeypote.va@gmail.com>
2026-01-14 10:37:55 +0100

    Add haptics to header actions and refresh grid modal

--- becap/Views/Home/Calendar/CalendarDetailView.swift
+++ becap/Views/Home/Calendar/CalendarDetailView.swift
@@ -291,11 +291,13 @@ struct CalendarDetailView: View {
                     }
 
                     GlassCircleIcon(systemName: "bell.fill")
+                        .hapticTap()
                         .onTapGesture {
                             showJokerBubble = false
                             showNotifSheet = true
                         }
                     GlassCircleIcon(systemName: "square.and.arrow.up.fill")
+                        .hapticTap()
                         .onTapGesture {
                             showJokerBubble = false
                             presentShareSheet()
@@ -324,6 +326,7 @@ struct CalendarDetailView: View {
                                     .accessibilityHidden(true)
                             }
                         }
+                        .hapticTap()
                         .onTapGesture {
                             showJokerBubble = false
                             showParticipantsSheet = true
