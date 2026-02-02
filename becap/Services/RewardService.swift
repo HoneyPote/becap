efa00087f6012aa9cceba88c9284f6027804f6dc
4n-iDiot <victor@derveaux.biz>
2026-02-02 23:49:06 +0100

    fix(test): changed testing firebase database to prod

--- becap/Services/RewardService.swift
+++ becap/Services/RewardService.swift
@@ -79,7 +79,7 @@ class RewardService {
 
     func persistProgress(_ progress: ParticipantProgress) async {
         do {
-            try db.collection("challengesTesting")
+            try db.collection("challenges")
                 .document(progress.challengeId)
                 .collection("participants")
                 .document(progress.id)
