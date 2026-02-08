5777e1c1cc3ee2ba7dbadda606c7c6a69177c4db
HoneyPote <honeypote.va@gmail.com>
2026-02-08 11:06:38 +0100

    Update preview to timelapse output

--- becap/ViewModels/CustomCameraViewModel.swift
+++ becap/ViewModels/CustomCameraViewModel.swift
@@ -432,6 +432,12 @@ extension CustomCameraViewModel: AVCapturePhotoCaptureDelegate, AVCaptureFileOut
                                      timelapseURL: URL?,
                                      processId: UUID) {
         let finalURL = timelapseURL ?? originalURL
+        DispatchQueue.main.async {
+            if self.currentProcessId == processId {
+                self.isProcessingTimelapse = false
+                self.capturedMedia = .video(ChallengeRawMedia.VideoRawData(url: finalURL, thumbnailImage: nil))
+            }
+        }
 
         generateThumbnailAsync(for: finalURL) { [weak self] thumbnail in
             guard let self else { return }
@@ -445,7 +451,6 @@ extension CustomCameraViewModel: AVCapturePhotoCaptureDelegate, AVCaptureFileOut
             }
 
             DispatchQueue.main.async {
-                self.isProcessingTimelapse = false
                 self.capturedMedia = .video(ChallengeRawMedia.VideoRawData(url: finalURL,
                                                                            thumbnailImage: thumbnail))
             }
