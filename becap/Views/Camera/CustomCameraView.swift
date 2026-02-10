652a822161d61693fa67e39ef59ab7e8897f2bb9
HoneyPote <honeypote.va@gmail.com>
2026-02-10 18:01:44 +0100

    Add front camera switch button

--- becap/Views/Camera/CustomCameraView.swift
+++ becap/Views/Camera/CustomCameraView.swift
@@ -156,13 +156,23 @@ struct CustomCameraView: View {
                 Spacer()
 
                 if viewModel.showFlashButton {
-                    Image(systemName: viewModel.flashIconName)
-                        .font(.title2)
-                        .foregroundColor(.white)
-                        .padding(10)
-                        .background(Color.black.opacity(0.4))
-                        .clipShape(Circle())
-                        .onTapGesture { viewModel.toggleFlash() }
+                    HStack(spacing: 12) {
+                        Image(systemName: "camera.rotate")
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Color.black.opacity(0.4))
+                            .clipShape(Circle())
+                            .onTapGesture { viewModel.switchCamera() }
+
+                        Image(systemName: viewModel.flashIconName)
+                            .font(.title2)
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Color.black.opacity(0.4))
+                            .clipShape(Circle())
+                            .onTapGesture { viewModel.toggleFlash() }
+                    }
                 }
             }
             .padding()
