c8ef59ac0554e09330fdea942105253decb75a04
Adam <adam.mabrouki@webwag.com>
2026-02-20 23:28:13 +0100

    Squashed commit of the following:
    commit f5333683478a897d87eb363b607920e884a6259c
    Author: Adam <adam.mabrouki@webwag.com>
    Date:   Fri Feb 20 23:27:47 2026 +0100
    
        resolve conflits

--- becap/Views/Settings/ParticipantMedalSection.swift
+++ becap/Views/Settings/ParticipantMedalSection.swift
@@ -58,7 +58,7 @@ struct ParticipantMedalSection: View {
 
         return grouped.map { (name, items) in
             let definition = MedalCatalog.definition(named: name)
-            MedalDisplayItem(name: name,
+            return MedalDisplayItem(name: name,
                              description: items.first?.description ?? "",
                              iconName: items.first?.iconName ?? "star.fill",
                              count: items.count,
