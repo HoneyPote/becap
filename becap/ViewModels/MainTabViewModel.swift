b89a782d87b3bacc87fa72dd0aa9ab15a677e7ff
Adam <adam.mabrouki@webwag.com>
2026-02-21 00:07:25 +0100

    Squashed commit of the following:
    commit 27bae179121567b4f6f266d6fc4252f915381801
    Author: Adam <adam.mabrouki@webwag.com>
    Date:   Sat Feb 21 00:06:11 2026 +0100
    
        resolve conflit

--- becap/ViewModels/MainTabViewModel.swift
+++ becap/ViewModels/MainTabViewModel.swift
@@ -60,7 +60,7 @@ class MainTabViewModel: ObservableObject {
 }
 
 // MARK: - Observers
-extension MainViewModel {
+extension MainTabViewModel {
     private func observeMedals() {
         alertManager.$currentMedals
             .receive(on: DispatchQueue.main)
